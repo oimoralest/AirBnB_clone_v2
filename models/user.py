4a600d17e45b77543ff379d71036c1fb039644b2
Fabkar <2003@holbertonschool.com>
2020-12-15 11:08:23 -0500

    add line with relationship place

--- models/user.py
+++ models/user.py
@@ -3,7 +3,7 @@
 from models.base_model import BaseModel, Base
 from os import getenv
 from sqlalchemy import Column, String, ForeignKey
-
+from sqlalchemy.orm import relationship
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
@@ -13,4 +13,4 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128))
         last_name = Column(String(128))
-
+        places = relationship("Place", backref="user", cascade="delete")
