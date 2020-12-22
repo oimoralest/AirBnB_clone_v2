5239196c38cc6ea1c2cd26b8041fb63db0d1d4b5
oimoralest <65910312+oimoralest@users.noreply.github.com>
2020-12-22 14:19:43 -0500

    Deleted tee and autoindex

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,8 +5,8 @@ apt-get upgrade
 apt-get install -y nginx
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo "Testing nginx configuration" | tee /data/web_static/releases/test/index.html
+echo "Testing nginx configuration" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-sed -i '/server_name _;/ a location /hbnb_static/ {\n\talias /data/web_static/current/;\n\tautoindex off;\n\t}'  /etc/nginx/sites-available/default
+sed -i '/server_name _;/ a location /hbnb_static/ {\n\talias /data/web_static/current/;\n\t}'  /etc/nginx/sites-available/default
 service nginx restart
