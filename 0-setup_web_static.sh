24175ec05d04faaa5bb7d082a89602d73c130cf9
oimoralest <oimoralest@gmail.com>
2020-12-22 14:11:32 -0500

    Script that configures a nginx server

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Configures a web server
+apt-get update
+apt-get upgrade
+apt-get install -y nginx
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Testing nginx configuration" | tee /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+sed -i '/server_name _;/ a location /hbnb_static/ {\n\talias /data/web_static/current/;\n\tautoindex off;\n\t}'  /etc/nginx/sites-available/default
+service nginx restart
