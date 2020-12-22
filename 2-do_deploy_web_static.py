f00378c63a90b9f555743113c6a36507d2733112
oimoralest <oimoralest@gmail.com>
2020-12-22 17:32:04 -0500

    Added fabric script that deploy new content

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Fabric script"""
+from fabric.api import put, run, env
+env.user = 'ubuntu'
+env.hosts = ['34.75.154.27', '34.75.211.153']
+
+
+def do_deploy(archive_path):
+    """Deploy new static content"""
+    try:
+        upload = put(archive_path, "/tmp/")
+        name = upload[0].split("/")[2].split(".")[0]
+        run("mkdir -p /data/web_static/releases/{}".format(name))
+        run("tar -xzf /tmp/{}.tgz -C "
+            "/data/web_static/releases/{}".format(name, name))
+        run("rm /tmp/{}.tgz".format(name))
+        run("mv /data/web_static/releases/{}/web_static/* "
+            "/data/web_static/releases/{}/".format(name, name))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(name))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{} "
+            "/data/web_static/current".format(name))
+        return True
+    except Exception:
+        return False
