e5ff5058d55a1b1036a1caca68ca80b929b1e38c
oimoralest <oimoralest@gmail.com>
2020-12-22 22:35:16 -0500

    Added restart nginx

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -10,7 +10,8 @@ exec { 'configuration':
   ln -sf /data/web_static/releases/test /data/web_static/current
   chown -R ubuntu:ubuntu /data/
   sed -i "/listen 80 default_server;/a \ \tlocation \/hbnb_static {\n\t\talias
-  \/data\/web_static\/current\/;\n}" /etc/nginx/sites-available/default',
+  \/data\/web_static\/current\/;\n}" /etc/nginx/sites-available/default
+  service nginx restart',
   provider => 'shell'
 }
 service { 'nginx':
