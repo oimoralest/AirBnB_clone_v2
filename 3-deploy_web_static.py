b28cadc35b57c5115ae66fd552ef8f173ed7b5a9
oimoralest <oimoralest@gmail.com>
2020-12-22 19:11:20 -0500

    Fixed

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -38,7 +38,7 @@ def do_deploy(archive_path):
         run("ln -s /data/web_static/releases/{} "
             "/data/web_static/current".format(name))
         return True
-    except FatalError:
+    except Exception:
         return False
 
 
