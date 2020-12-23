492b5749e5799dd95690b75fbc6a063b8c46d21c
oimoralest <oimoralest@gmail.com>
2020-12-22 21:40:01 -0500

    Modified

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # Configures a web server
 apt-get update
-apt-get upgrade
-apt-get install -y nginx
+apt-get update
+apt-get -y install nginx
 mkdir -p /data/web_static/releases/test
 mkdir -p /data/web_static/shared
-echo "Testing nginx configuration" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+echo "Testing" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-sed -i '/server_name _;/ a \ \tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}'  /etc/nginx/sites-available/default
+sed -i "/listen 80 default_server;/a \ \tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current\/;\n}" /etc/nginx/sites-available/default
 service nginx restart
