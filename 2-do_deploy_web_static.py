583b4ec68a1aa7125b0f6426d4fc0b0bc91e73b8
oimoralest <oimoralest@gmail.com>
2020-12-22 21:36:10 -0500

    Added sudo

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -13,16 +13,16 @@ def do_deploy(archive_path):
     try:
         upload = put(archive_path, "/tmp/")
         name = upload[0].split("/")[2].split(".")[0]
-        run("mkdir -p /data/web_static/releases/{}".format(name))
-        run("tar -xzf /tmp/{}.tgz -C "
+        run("sudo mkdir -p /data/web_static/releases/{}".format(name))
+        run("sudo tar -xzf /tmp/{}.tgz -C "
             "/data/web_static/releases/{}".format(name, name))
-        run("rm /tmp/{}.tgz".format(name))
-        run("mv /data/web_static/releases/{}/web_static/* "
+        run("sudo rm /tmp/{}.tgz".format(name))
+        run("sudo mv /data/web_static/releases/{}/web_static/* "
             "/data/web_static/releases/{}/".format(name, name))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{} "
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s /data/web_static/releases/{} "
             "/data/web_static/current".format(name))
-        run("rm -rf /data/web_static/current/web_static".format(name))
+        run("sudo rm -rf /data/web_static/current/web_static".format(name))
         return True
     except:
         return False
