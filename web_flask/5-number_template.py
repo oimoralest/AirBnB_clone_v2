3206b938440655467a92c4126d8a29f0617175fc
oimoralest <oimoralest@gmail.com>
2021-01-19 15:36:21 -0500

    Added script that starts a Flask web application

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+"""This script starts a flask web application
+"""
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def helloHbtn():
+    """This function returns a content for the specified route
+
+    Returns:
+        string: content to return
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbtn():
+    """This function returns a content for the specified route
+
+    Returns:
+        string: content to return
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def variableText(text=''):
+    """This function returns a content for the specified route
+
+    Args:
+        text (str, optional): Input text. Defaults to ''.
+
+    Returns:
+        string: content to return
+    """
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythonIs(text=''):
+    """This function returns a content for the specified route
+
+    Args:
+        text (str, optional): Input text. Defaults to 'is cool'.
+
+    Returns:
+        string: content to return
+    """
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def displayInt(n):
+    """This function returns a content for the specified route only if n is an
+    integer
+
+    Args:
+        n (int): Input integer
+
+    Returns:
+        string: content to return
+    """
+    return str(n) + ' is a number'
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def displayTemplate(n):
+    """This function returns an HTML template only if n is an integer
+
+    Args:
+        n (int): Input integer
+
+    Returns:
+        strin: HTML template
+    """
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
