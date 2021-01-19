9c4d85c1e4f16420fa8cbe82e7320d2c7b3f6ed3
oimoralest <oimoralest@gmail.com>
2021-01-19 15:26:56 -0500

    Added script that starts a Flask web application

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""This script starts a flask web application
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def helloHbtn():
+    """This function return a content for the specified route
+
+    Returns:
+        string: content to return
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbtn():
+    """This function return a content for the specified route
+
+    Returns:
+        string: content to return
+    """
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
